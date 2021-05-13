3e31c8d83c1a14599dd096a7e97c9bdd1940330a
Tim Gonzales <tgonzales@avioconsulting.com>
2021-05-13 07:52:30 -0500

    Inititalizing starter class

---
+++ src/main/java/com/avioconsulting/opentelemetry/OpenTelemetryStarter.java
@@ -0,0 +1,15 @@
+package com.avioconsulting.opentelemetry;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OpenTelemetryStarter {
+
+    private Logger logger = LoggerFactory.getLogger(OpenTelemetryStarter.class);
+
+    public OpenTelemetryStarter() {
+
+        logger.debug("Initialising OpenTelemetry Mule 4 Agent");
+
+    }
+}
