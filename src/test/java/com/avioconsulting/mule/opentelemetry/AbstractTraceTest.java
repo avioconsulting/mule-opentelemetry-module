18ebb7e3abef3e8d6d4a122f05d7d323e02efa3d
Manik Magar <manik.magar@gmail.com>
2022-01-10 14:47:18 -0500

    test refactor

---
+++ src/test/java/com/avioconsulting/mule/opentelemetry/AbstractTraceTest.java
@@ -0,0 +1,66 @@
+package com.avioconsulting.mule.opentelemetry;
+
+import org.mule.functional.junit4.MuleArtifactFunctionalTestCase;
+import org.mule.runtime.core.api.event.CoreEvent;
+import org.mule.tck.probe.JUnitLambdaProbe;
+import org.mule.tck.probe.PollingProber;
+import org.mule.test.runner.ArtifactClassLoaderRunnerConfig;
+
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.atomic.AtomicReference;
+
+@ArtifactClassLoaderRunnerConfig(applicationSharedRuntimeLibs = {
+        "com.avioconsulting:open-telemetry-mule4-agent",
+        "io.opentelemetry:opentelemetry-api",
+        "io.opentelemetry:opentelemetry-sdk-extension-autoconfigure",
+        "io.opentelemetry:opentelemetry-sdk-extension-autoconfigure-spi",
+        "io.opentelemetry:opentelemetry-sdk",
+        "io.opentelemetry:opentelemetry-semconv",
+        "io.opentelemetry:opentelemetry-sdk-common",
+        "io.opentelemetry:opentelemetry-context",
+        "io.opentelemetry:opentelemetry-sdk-metrics",
+        "io.opentelemetry:opentelemetry-api-metrics",
+        "io.opentelemetry:opentelemetry-sdk-trace",
+        "io.opentelemetry:opentelemetry-exporter-logging",
+        "io.opentelemetry:opentelemetry-exporter-zipkin",
+        "io.zipkin.zipkin2:zipkin",
+        "io.zipkin.reporter2:zipkin-reporter",
+        "io.zipkin.reporter2:zipkin-sender-okhttp3",
+        "com.squareup.okio:okio",
+        "com.squareup.okhttp3:okhttp"
+
+})
+public abstract class AbstractTraceTest  extends MuleArtifactFunctionalTestCase {
+
+    protected static final java.util.Queue<CoreEvent> CAPTURED = new ConcurrentLinkedDeque<>();
+
+    @Override
+    protected void doSetUpBeforeMuleContextCreation() throws Exception {
+        super.doSetUpBeforeMuleContextCreation();
+        System.setProperty(TEST_TIMEOUT_SYSTEM_PROPERTY, "120_000_000");
+        System.setProperty("otel.traces.exporter", "logging");
+        System.setProperty("otel.resource.attributes", "deployment.environment=test,service.name=test-flows");
+        System.setProperty("otel.metrics.exporter", "none");
+    }
+
+    protected CoreEvent getCapturedEvent(long timeout, String failureDescription) {
+        AtomicReference<CoreEvent> value = new AtomicReference<>();
+        new PollingProber(timeout, 100)
+                .check(
+                        new JUnitLambdaProbe(
+                                () -> {
+                                    synchronized (CAPTURED) {
+                                        CoreEvent capturedEvent = CAPTURED.poll();
+                                        if (capturedEvent != null) {
+                                            value.set(capturedEvent);
+                                            return true;
+                                        }
+                                        return false;
+                                    }
+                                },
+                                failureDescription));
+
+        return value.get();
+    }
+
+}
--- src/test/java/com/avioconsulting/mule/opentelemetry/MuleFlowTraceTest.java
+++ src/test/java/com/avioconsulting/mule/opentelemetry/MuleFlowTraceTest.java
@@ -6,43 +6,18 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.junit.Rule;
 import org.junit.Test;
-import org.mule.functional.junit4.MuleArtifactFunctionalTestCase;
 import org.mule.runtime.core.api.event.CoreEvent;
 import org.mule.tck.junit4.rule.DynamicPort;
-import org.mule.tck.probe.JUnitLambdaProbe;
-import org.mule.tck.probe.PollingProber;
-import org.mule.test.runner.ArtifactClassLoaderRunnerConfig;
 
 import java.io.IOException;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentLinkedDeque;
-import java.util.concurrent.atomic.AtomicReference;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-@ArtifactClassLoaderRunnerConfig(applicationSharedRuntimeLibs = {
-        "com.avioconsulting:open-telemetry-mule4-agent",
-        "io.opentelemetry:opentelemetry-api",
-        "io.opentelemetry:opentelemetry-sdk-extension-autoconfigure",
-        "io.opentelemetry:opentelemetry-sdk-extension-autoconfigure-spi",
-        "io.opentelemetry:opentelemetry-sdk",
-        "io.opentelemetry:opentelemetry-semconv",
-        "io.opentelemetry:opentelemetry-sdk-common",
-        "io.opentelemetry:opentelemetry-context",
-        "io.opentelemetry:opentelemetry-sdk-metrics",
-        "io.opentelemetry:opentelemetry-api-metrics",
-        "io.opentelemetry:opentelemetry-sdk-trace",
-        "io.opentelemetry:opentelemetry-exporter-otlp",
-        "io.opentelemetry:opentelemetry-exporter-zipkin",
-        "io.zipkin.zipkin2:zipkin",
-        "io.zipkin.reporter2:zipkin-reporter",
-        "io.zipkin.reporter2:zipkin-sender-okhttp3",
-        "com.squareup.okio:okio",
-        "com.squareup.okhttp3:okhttp"
+public class MuleFlowTraceTest extends AbstractTraceTest {
 
-})
-public class MuleFlowTraceTest extends MuleArtifactFunctionalTestCase {
     protected static final java.util.Queue<CoreEvent> CAPTURED = new ConcurrentLinkedDeque<>();
     @Rule
     public DynamicPort serverPort = new DynamicPort("http.port");
@@ -52,32 +27,13 @@ public class MuleFlowTraceTest extends MuleArtifactFunctionalTestCase {
         return "SimpleFlowTest.xml";
     }
 
-
     @Override
     protected void doSetUpBeforeMuleContextCreation() throws Exception {
         super.doSetUpBeforeMuleContextCreation();
         System.setProperty(TEST_TIMEOUT_SYSTEM_PROPERTY, "120_000_000");
-    }
-
-
-    protected CoreEvent getCapturedEvent(long timeout, String failureDescription) {
-        AtomicReference<CoreEvent> value = new AtomicReference<>();
-        new PollingProber(timeout, 100)
-                .check(
-                        new JUnitLambdaProbe(
-                                () -> {
-                                    synchronized (CAPTURED) {
-                                        CoreEvent capturedEvent = CAPTURED.poll();
-                                        if (capturedEvent != null) {
-                                            value.set(capturedEvent);
-                                            return true;
-                                        }
-                                        return false;
-                                    }
-                                },
-                                failureDescription));
-
-        return value.get();
+        System.setProperty("otel.traces.exporter", "logging");
+        System.setProperty("otel.resource.attributes", "deployment.environment=test,service.name=test-flows");
+        System.setProperty("otel.metrics.exporter", "none");
     }
 
     @Test
@@ -90,7 +46,6 @@ public class MuleFlowTraceTest extends MuleArtifactFunctionalTestCase {
     @Test
     public void exceptionFlowTest() throws Exception {
         String correlationId = UUID.randomUUID().toString();
-        //TODO: StatusCode should be 404 but some DWL error is causing 500.
         sendRequest(correlationId, "exception", 500);
     }
 
@@ -105,8 +60,7 @@ public class MuleFlowTraceTest extends MuleArtifactFunctionalTestCase {
 //        getRequest.addHeader("traceparent", "00-3e864597bcb2431935133b0dec678ed4-f75931b2493ab2b2-00");
         try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
             try (CloseableHttpResponse response = httpClient.execute(getRequest)) {
-                //Hardcoding to 500, DWL error in test.
-                //assertThat(response.getStatusLine().getStatusCode()).isEqualTo(expectedStatus);
+                assertThat(response.getStatusLine().getStatusCode()).isEqualTo(expectedStatus);
             }
         }
     }
