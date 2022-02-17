e7a106b8b97190651cef3d1eb52c2f1d9126e912
Manik Magar <manik.magar@gmail.com>
2022-02-16 22:52:49 -0500

    Add Mule Resource and Mule Attribute Keys

---
+++ src/main/java/com/avioconsulting/mule/opentelemetry/internal/opentelemetry/sdk/MuleResource.java
@@ -0,0 +1,47 @@
+package com.avioconsulting.mule.opentelemetry.internal.opentelemetry.sdk;
+
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.common.AttributesBuilder;
+import io.opentelemetry.sdk.resources.Resource;
+
+import static com.avioconsulting.mule.opentelemetry.internal.opentelemetry.sdk.SemanticAttributes.*;
+
+/**
+ * Creates an OpenTelemetry @{@link Resource} that adds Mule Runtime specific
+ * attributes in trace data.
+ */
+public class MuleResource {
+  private static final Resource INSTANCE = buildResource();
+
+  public static Resource get() {
+    return INSTANCE;
+  }
+
+  private static Resource buildResource() {
+    AttributesBuilder builder = Attributes.builder();
+    addAttribute("mule.home", builder, MULE_HOME);
+    addAttribute("csorganization.id", builder, MULE_CSORGANIZATION_ID);
+    addAttribute("environment.id", builder, MULE_ENVIRONMENT_ID);
+    addAttribute("environment.type", builder, MULE_ENVIRONMENT_TYPE);
+    addAttribute("worker.id", builder, MULE_WORKER_ID);
+    addAttribute("domain", builder, MULE_APP_DOMAIN);
+    addAttribute("fullDomain", builder, MULE_APP_FULL_DOMAIN);
+    addAttribute("application.aws.region", builder, MULE_APP_AWS_REGION);
+    addAttribute("mule.env", builder, MULE_ENV);
+    Attributes build = builder.build();
+    return Resource.create(build);
+  }
+
+  private static void addAttribute(String key, AttributesBuilder builder,
+      AttributeKey<String> attributeKey) {
+    String value = System.getProperty(key);
+    if (value != null) {
+      builder.put(attributeKey, value);
+    }
+  }
+
+  private MuleResource() {
+
+  }
+}
---
+++ src/main/java/com/avioconsulting/mule/opentelemetry/internal/opentelemetry/sdk/MuleResourceProvider.java
@@ -0,0 +1,16 @@
+package com.avioconsulting.mule.opentelemetry.internal.opentelemetry.sdk;
+
+import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
+import io.opentelemetry.sdk.autoconfigure.spi.ResourceProvider;
+import io.opentelemetry.sdk.resources.Resource;
+
+public class MuleResourceProvider implements ResourceProvider {
+
+  public MuleResourceProvider() {
+  }
+
+  @Override
+  public Resource createResource(ConfigProperties configProperties) {
+    return MuleResource.get();
+  }
+}
