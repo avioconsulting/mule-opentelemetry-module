ba09128cb94bc4d2e5b3b43ad04ee07b25b0c9a5
Manik Magar <manik.magar@gmail.com>
2022-09-12 10:39:56 -0400

    Add request compression param for OTLP exporter (#47)

--- src/main/java/com/avioconsulting/mule/opentelemetry/api/config/Attribute.java
+++ src/main/java/com/avioconsulting/mule/opentelemetry/api/config/Attribute.java
@@ -4,4 +4,12 @@ import org.mule.runtime.extension.api.annotation.Alias;
 
 @Alias("attribute")
 public class Attribute extends KeyValuePair {
+
+  public Attribute() {
+    super();
+  }
+
+  public Attribute(String key, String value) {
+    super(key, value);
+  }
 }
