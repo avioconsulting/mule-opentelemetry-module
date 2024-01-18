4e7c580f5a2a959da41fb5eccda1a5178a7a0924
Manik Magar <mmagar@avioconsulting.com>
2024-01-18 09:55:47 +0530

    feat: add long trace and span id to context (#147)

--- src/main/java/com/avioconsulting/mule/opentelemetry/internal/store/TransactionContext.java
+++ src/main/java/com/avioconsulting/mule/opentelemetry/internal/store/TransactionContext.java
@@ -1,22 +1,63 @@
 package com.avioconsulting.mule.opentelemetry.internal.store;
 
+import com.avioconsulting.mule.opentelemetry.internal.util.EncodingUtil;
 import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanId;
+import io.opentelemetry.api.trace.TraceId;
 import io.opentelemetry.context.Context;
 
 import java.util.Objects;
 
 public class TransactionContext {
-  private Context context;
-  private String spanId;
+  private Context context = Context.current();
+
+  /**
+   * Get the 16-hex-character lowercase string span id
+   */
+  private String spanId = SpanId.getInvalid();
+  /**
+   * Trace id is a 32-hex-character lowercase string built with two
+   * 16-hex-character lowercase strings,
+   * each representing a long value encoded to hex.
+   * First 16 characters represent High-long and the last 16 characters represent
+   * Low-long values making the trace id.
+   */
+  private String traceId = TraceId.getInvalid();
+
+  /**
+   * This method returns the String formatted Long value of span id. See
+   * {@link TransactionContext#getSpanId()}.
+   * Example: Span Id - "53f9aa133a283c1a"
+   * Long Low Id - "6051054573905787930"
+   */
+  private String spanIdLong = "0";
+
+  /**
+   * This method returns the String formatted Long value of the Low part of the
+   * trace id. See {@link TransactionContext#getTraceId()}.
+   * Example: Trace Id - fbc14552c62fbabc6a4bc6817cd983ce
+   * High-part - "fbc14552c62fbabc"
+   * Low-part - "6a4bc6817cd983ce"
+   * Long Low Id - "7659433850721371086"
+   */
+  private String traceIdLongLowPart = "0";
 
   public static TransactionContext of(Span span) {
-    return new TransactionContext()
+    TransactionContext transactionContext = new TransactionContext()
         .setContext(span.storeInContext(Context.current()))
-        .setSpanId(span.getSpanContext().getSpanId());
+        .setSpanId(span.getSpanContext().getSpanId())
+        .setTraceId(span.getSpanContext().getTraceId());
+    if (SpanId.isValid(transactionContext.getSpanId())) {
+      transactionContext.setSpanIdLong(EncodingUtil.longFromBase16Hex(transactionContext.getSpanId()));
+    }
+    if (TraceId.isValid(transactionContext.getTraceId())) {
+      transactionContext.setTraceIdLongLowPart(EncodingUtil.traceIdLong(transactionContext.getTraceId())[1]);
+    }
+    return transactionContext;
   }
 
   public static TransactionContext current() {
-    return new TransactionContext().setContext(Context.current());
+    return new TransactionContext();
   }
 
   public Context getContext() {
@@ -37,6 +78,33 @@ public class TransactionContext {
     return this;
   }
 
+  public String getTraceId() {
+    return traceId;
+  }
+
+  public TransactionContext setTraceId(String traceId) {
+    this.traceId = traceId;
+    return this;
+  }
+
+  public String getSpanIdLong() {
+    return spanIdLong;
+  }
+
+  public TransactionContext setSpanIdLong(String spanIdLong) {
+    this.spanIdLong = spanIdLong;
+    return this;
+  }
+
+  public String getTraceIdLongLowPart() {
+    return traceIdLongLowPart;
+  }
+
+  public TransactionContext setTraceIdLongLowPart(String traceIdLongLowPart) {
+    this.traceIdLongLowPart = traceIdLongLowPart;
+    return this;
+  }
+
   @Override
   public boolean equals(Object o) {
     if (this == o)
@@ -44,11 +112,14 @@ public class TransactionContext {
     if (o == null || getClass() != o.getClass())
       return false;
     TransactionContext that = (TransactionContext) o;
-    return Objects.equals(getContext(), that.getContext()) && Objects.equals(getSpanId(), that.getSpanId());
+    return Objects.equals(getContext(), that.getContext()) && Objects.equals(getSpanId(), that.getSpanId())
+        && Objects.equals(getTraceId(), that.getTraceId())
+        && Objects.equals(getSpanIdLong(), that.getSpanIdLong())
+        && Objects.equals(getTraceIdLongLowPart(), that.getTraceIdLongLowPart());
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(getContext(), getSpanId());
+    return Objects.hash(getContext(), getSpanId(), getTraceId(), getSpanIdLong(), getTraceIdLongLowPart());
   }
 }
---
+++ src/test/java/com/avioconsulting/mule/opentelemetry/internal/store/TransactionContextTest.java
@@ -0,0 +1,46 @@
+package com.avioconsulting.mule.opentelemetry.internal.store;
+
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanContext;
+import io.opentelemetry.context.Context;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class TransactionContextTest {
+
+  @Test
+  public void current() {
+    TransactionContext transactionContext = TransactionContext.current();
+    Assertions.assertThat(transactionContext)
+        .extracting("spanId", "spanIdLong", "traceId", "traceIdLongLowPart")
+        .containsOnly("0000000000000000", "0", "00000000000000000000000000000000", "0");
+    Assertions.assertThat(transactionContext.getContext())
+        .as("Current Context")
+        .isNotNull();
+  }
+
+  @Test
+  public void ofValidSpan() {
+    Span span = mock(Span.class);
+    SpanContext spanContext = mock(SpanContext.class);
+    when(spanContext.getSpanId()).thenReturn("53f9aa133a283c1a");
+    when(spanContext.getTraceId()).thenReturn("fbc14552c62fbabc6a4bc6817cd983ce");
+    when(span.getSpanContext()).thenReturn(spanContext);
+    when(span.storeInContext(any(Context.class))).thenReturn(Context.current());
+
+    TransactionContext transactionContext = TransactionContext.of(span);
+    Assertions.assertThat(transactionContext)
+        .extracting("spanId", "spanIdLong", "traceId", "traceIdLongLowPart")
+        .containsOnly("53f9aa133a283c1a", "6051054573905787930", "fbc14552c62fbabc6a4bc6817cd983ce",
+            "7659433850721371086");
+    Assertions.assertThat(transactionContext.getContext())
+        .as("Current Context")
+        .isNotNull();
+  }
+
+}
