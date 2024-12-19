45b678de97edf00a9b0175259e56df1db3771e2c
Manik Magar <mmagar@avioconsulting.com>
2024-12-19 14:15:58 +0530

    test: add unit tests

--- src/test/java/com/avioconsulting/mule/opentelemetry/internal/processor/AbstractProcessorComponentTest.java
+++ src/test/java/com/avioconsulting/mule/opentelemetry/internal/processor/AbstractProcessorComponentTest.java
@@ -1,6 +1,15 @@
 package com.avioconsulting.mule.opentelemetry.internal.processor;
 
+import com.avioconsulting.mule.opentelemetry.api.config.ExporterConfiguration;
+import com.avioconsulting.mule.opentelemetry.api.config.OpenTelemetryResource;
+import com.avioconsulting.mule.opentelemetry.api.config.SpanProcessorConfiguration;
+import com.avioconsulting.mule.opentelemetry.api.config.exporter.OpenTelemetryExporter;
 import com.avioconsulting.mule.opentelemetry.internal.AbstractInternalTest;
+import com.avioconsulting.mule.opentelemetry.internal.config.OpenTelemetryConfigWrapper;
+import com.avioconsulting.mule.opentelemetry.internal.config.OpenTelemetryConfiguration;
+import com.avioconsulting.mule.opentelemetry.internal.connection.OpenTelemetryConnection;
+import io.opentelemetry.sdk.testing.junit4.OpenTelemetryRule;
+import org.junit.ClassRule;
 import org.mockito.Mockito;
 import org.mule.runtime.api.component.Component;
 import org.mule.runtime.api.component.ComponentIdentifier;
@@ -15,9 +24,42 @@ import javax.xml.namespace.QName;
 import java.util.Map;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
 public abstract class AbstractProcessorComponentTest extends AbstractInternalTest {
+
+  @ClassRule
+  public static OpenTelemetryRule openTelemetryRule = OpenTelemetryRule.create();
+
+  /**
+   * Retrieves an OpenTelemetry connection with the specified configuration
+   * settings.
+   *
+   * Returns a {@link org.mockito.Spy} of created {@link OpenTelemetryConnection}
+   * so that additional mock behavior can
+   * be set by the caller.
+   *
+   * @return a Spy of OpenTelemetryConnection instance with the specified
+   *         configuration settings
+   */
+  protected OpenTelemetryConnection getOpenTelemetryConnection() {
+    OpenTelemetryResource resource = mock(OpenTelemetryResource.class);
+    ExporterConfiguration exporterConfig = mock(ExporterConfiguration.class);
+    OpenTelemetryExporter exporter = mock(OpenTelemetryExporter.class);
+    when(exporterConfig.getExporter()).thenReturn(exporter);
+    SpanProcessorConfiguration spc = mock(SpanProcessorConfiguration.class);
+    OpenTelemetryConfiguration configuration = mock(OpenTelemetryConfiguration.class);
+    when(configuration.getResource()).thenReturn(resource);
+    when(configuration.getExporterConfiguration()).thenReturn(exporterConfig);
+    when(configuration.getSpanProcessorConfiguration()).thenReturn(spc);
+    when(configuration.isTurnOffMetrics()).thenReturn(true);
+    OpenTelemetryConfigWrapper wrapper = new OpenTelemetryConfigWrapper(configuration);
+    OpenTelemetryConnection instance = OpenTelemetryConnection.getInstance(wrapper);
+    OpenTelemetryConnection spy = spy(instance);
+    spy.withOpenTelemetry(openTelemetryRule.getOpenTelemetry());
+    return spy;
+  }
 
   protected ComponentIdentifier getMockedIdentifier(String namespace, String name) {
     ComponentIdentifier identifier = mock(ComponentIdentifier.class);
@@ -83,6 +125,19 @@ public abstract class AbstractProcessorComponentTest extends AbstractInternalTes
 
     TypedComponentIdentifier typedComponentIdentifier = mock(TypedComponentIdentifier.class);
     ComponentIdentifier mockedIdentifier = getMockedIdentifier(namespace, name);
+    when(typedComponentIdentifier.getIdentifier()).thenReturn(mockedIdentifier);
+    when(componentLocation.getComponentIdentifier()).thenReturn(typedComponentIdentifier);
+
+    return componentLocation;
+  }
+
+  protected ComponentLocation getFlowLocation(String flowName) {
+    ComponentLocation componentLocation = mock(ComponentLocation.class);
+    when(componentLocation.getLocation()).thenReturn(flowName);
+    when(componentLocation.getRootContainerName()).thenReturn(flowName);
+
+    TypedComponentIdentifier typedComponentIdentifier = mock(TypedComponentIdentifier.class);
+    ComponentIdentifier mockedIdentifier = getMockedIdentifier("mule", "flow");
     when(typedComponentIdentifier.getIdentifier()).thenReturn(mockedIdentifier);
     when(componentLocation.getComponentIdentifier()).thenReturn(typedComponentIdentifier);
 
---
+++ src/test/java/com/avioconsulting/mule/opentelemetry/internal/processor/MuleNotificationProcessorContextExtractorTest.java
@@ -0,0 +1,104 @@
+package com.avioconsulting.mule.opentelemetry.internal.processor;
+
+import com.avioconsulting.mule.opentelemetry.api.config.TraceLevelConfiguration;
+import com.avioconsulting.mule.opentelemetry.api.traces.TraceComponent;
+import com.avioconsulting.mule.opentelemetry.internal.connection.OpenTelemetryConnection;
+import io.opentelemetry.api.trace.Span;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.ArgumentCaptor;
+import org.mule.runtime.api.component.Component;
+import org.mule.runtime.api.component.location.ComponentLocation;
+import org.mule.runtime.api.component.location.ConfigurationComponentLocator;
+import org.mule.runtime.api.el.BindingContext;
+import org.mule.runtime.api.event.Event;
+import org.mule.runtime.api.event.EventContext;
+import org.mule.runtime.api.lifecycle.InitialisationException;
+import org.mule.runtime.api.message.Message;
+import org.mule.runtime.api.metadata.TypedValue;
+import org.mule.runtime.api.notification.EnrichedNotificationInfo;
+import org.mule.runtime.api.notification.PipelineMessageNotification;
+import org.mule.runtime.core.api.el.ExpressionManager;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+@RunWith(Parameterized.class)
+public class MuleNotificationProcessorContextExtractorTest extends AbstractProcessorComponentTest {
+
+  private ConfigurationComponentLocator configurationComponentLocator = mock(ConfigurationComponentLocator.class);
+
+  @Parameterized.Parameter(value = 0)
+  public static String expressionText;
+
+  @Parameterized.Parameters()
+  public static Collection<String> data() {
+    return Arrays.asList("#[attributes.headers]",
+        "#[attributes.properties]",
+        "#[attributes.properties.userProperties]", "#[payload.message.messageAttributes]");
+  }
+
+  @Test
+  public void handleFlowStartEvent_generic_context_extraction() throws InitialisationException {
+    Event event = mock(Event.class);
+    when(event.getCorrelationId()).thenReturn("testCorrelationId");
+    EventContext eventContext = mock(EventContext.class);
+    when(eventContext.getId()).thenReturn("testEventContextId");
+    when(event.getContext()).thenReturn(eventContext);
+    BindingContext bindingContext = mock(BindingContext.class);
+    when(event.asBindingContext()).thenReturn(bindingContext);
+
+    Map<String, Object> contextHolderMap = new HashMap<>();
+    contextHolderMap.put("traceparent", "00-436b61977bfcd902672d651c1b84e2f3-624994541baf73d4-01");
+
+    Map<String, Object> placeholder = new HashMap<>();
+    Message message = getMessage(placeholder);
+    when(event.getMessage()).thenReturn(message);
+    String flowName = "mule-sample-flow";
+    ComponentLocation componentLocation = getFlowLocation(flowName);
+    Component component = getComponent(componentLocation, Collections.emptyMap(), "mule", "flow");
+    Exception exception = mock(Exception.class);
+
+    EnrichedNotificationInfo info = EnrichedNotificationInfo.createInfo(event, exception, component);
+    PipelineMessageNotification pipelineMessageNotification = new PipelineMessageNotification(info, flowName,
+        PipelineMessageNotification.PROCESS_START);
+
+    OpenTelemetryConnection connection = getOpenTelemetryConnection();
+    ExpressionManager expressionManager = mock(ExpressionManager.class);
+    TypedValue<?> tTypedValue = TypedValue.of(contextHolderMap);
+    doReturn(tTypedValue).when(expressionManager).evaluate(eq(expressionText), any(BindingContext.class));
+    doReturn(expressionManager).when(connection).getExpressionManager();
+
+    ArgumentCaptor<TraceComponent> captor = ArgumentCaptor.forClass(TraceComponent.class);
+    doNothing().when(connection).startTransaction(captor.capture());
+
+    MuleNotificationProcessor notificationProcessor = new MuleNotificationProcessor(configurationComponentLocator);
+    notificationProcessor.init(connection, new TraceLevelConfiguration(false, Collections.emptyList()));
+    notificationProcessor.handleFlowStartEvent(pipelineMessageNotification);
+
+    TraceComponent value = captor.getValue();
+    assertThat(value).isNotNull();
+
+    Span span = Span.fromContext(value.getContext());
+    assertThat(span.getSpanContext())
+        .isNotNull()
+        .extracting("traceId", "spanId")
+        .as("Context extracted from " + expressionText)
+        .containsExactlyInAnyOrder("436b61977bfcd902672d651c1b84e2f3", "624994541baf73d4");
+  }
+
+}
