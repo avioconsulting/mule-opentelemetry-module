262550417f7665179ca256d83451ac8c002ce79e
Manik Magar <mmagar@avioconsulting.com>
2025-02-12 10:26:08 +0530

    fix: OTEL version in URL and echo info

--- examples/otel-collector/simple-collector/download.sh
+++ examples/otel-collector/simple-collector/download.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 
+# Usage: ./download.sh <release_version>
+# Example: ./download.sh 0.119.0
+
 if [ -z "$1" ]; then
   echo "Error: No version provided."
+  echo "Usage: $0 <release version>"
   exit 1
 fi
 
 VERSION=$1
 echo "Downloading otel-contrib collector version $VERSION"
 OTEL_FILE="otelcol-contrib_${VERSION}_linux_amd64.tar.gz"
-OTEL_URL="https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.119.0/${OTEL_FILE}"
+OTEL_URL="https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v${VERSION}/${OTEL_FILE}"
+
+echo "Downloading $OTEL_FILE from:"
+echo "$OTEL_URL"
+
 curl --proto '=https' --tlsv1.2 -fOL $OTEL_URL
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to download $OTEL_FILE"
+  exit 1
+fi
+
+echo "Extracting $OTEL_FILE..."
 tar -xvf $OTEL_FILE
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to extract $OTEL_FILE"
+  exit 1
+fi
+
+echo "Download and extraction complete."
